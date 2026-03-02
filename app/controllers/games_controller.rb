d57e160522ee4e2d9afd1eb9dac152905ec167e9
Mark Salvino <mosalvino@gmail.com>
2026-03-02 10:13:57 -0800

    Refactor and clean up

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,19 +6,22 @@ class GamesController < ApplicationController
 
   def play
     @game = GameState.last || GameState.create
-    mode = params[:mode] || 'friend'
-    # Set starting player for friend mode if specified
-    if params[:player] && @game.board == '---------'
-      @game.current_player = params[:player]
+    game_mode = params[:mode] || 'friend'
+    starting_player = params[:player]
+
+    if starting_player && @game.board == '---------'
+      @game.current_player = starting_player
       @game.save
     end
-    if params[:position]
-      @game.play_move(params[:position].to_i)
-      # Only trigger CPU move in single-player mode
-      if mode == 'single' && @game.status == 'ongoing' && @game.current_player == 'O'
-        @game.play_cpu_move('O')
+
+    move_position = params[:position]&.to_i
+    if move_position
+      @game.make_move(move_position)
+      if game_mode == 'single' && @game.status == 'ongoing' && @game.current_player == 'O'
+        @game.make_cpu_move('O')
       end
     end
+
     render :play
   end
 
--- app/models/game_state.rb
+++ app/models/game_state.rb
@@ -1,97 +1,31 @@
 class GameState < ApplicationRecord
-	after_initialize :init_board
+	after_initialize :initialize_board
 
-	def init_board
+	def initialize_board
 		self.board ||= '---------'
 		self.current_player ||= 'X'
 		self.status ||= 'ongoing'
 	end
 
-	def board_array
-		self.board.chars.each_slice(3).to_a
+	def board_matrix
+		board.chars.each_slice(3).to_a
 	end
 
-	def play_move(position)
-		return false unless self.status == 'ongoing' && self.board[position] == '-'
-		self.board[position] = self.current_player
-		self.current_player = self.current_player == 'X' ? 'O' : 'X'
-		self.status = check_status
+	def make_move(position)
+		return false unless status == 'ongoing' && board[position] == '-'
+		board[position] = current_player
+		self.current_player = current_player == 'X' ? 'O' : 'X'
+		self.status = game_status
 		save
 	end
 
-	def check_status
-		lines = [
-			[0,1,2], [3,4,5], [6,7,8], # rows
-			[0,3,6], [1,4,7], [2,5,8], # columns
-			[0,4,8], [2,4,6]           # diagonals
+	def game_status
+		win_lines = [
+			[0, 1, 2], [3, 4, 5], [6, 7, 8],
+			[0, 3, 6], [1, 4, 7], [2, 5, 8],
+			[0, 4, 8], [2, 4, 6]
 		]
-		lines.each do |line|
-			values = line.map { |i| self.board[i] }
-			return "#{values[0]} wins" if values.uniq.length == 1 && values[0] != '-'
-		end
-		return 'draw' unless self.board.include?('-')
-		'ongoing'
-	end
-
-	# Returns the best move for the CPU using minimax
-	def best_move(cpu_player = 'O')
-		moves = available_moves
-		best_score = -Float::INFINITY
-		move = nil
-		moves.each do |pos|
-			board_copy = self.board.dup
-			board_copy[pos] = cpu_player
-			score = minimax(board_copy, false, cpu_player, cpu_player == 'X' ? 'O' : 'X')
-			if score > best_score
-				best_score = score
-				move = pos
-			end
-		end
-		move
-	end
-
-	# Minimax algorithm implementation
-	def minimax(board, is_maximizing, cpu_player, human_player)
-		result = static_check_status(board)
-		return 1 if result == "#{cpu_player} wins"
-		return -1 if result == "#{human_player} wins"
-		return 0 if result == 'draw'
-
-		if is_maximizing
-			best_score = -Float::INFINITY
-			available_moves(board).each do |pos|
-				board_copy = board.dup
-				board_copy[pos] = cpu_player
-				score = minimax(board_copy, false, cpu_player, human_player)
-				best_score = [score, best_score].max
-			end
-			best_score
-		else
-			best_score = Float::INFINITY
-			available_moves(board).each do |pos|
-				board_copy = board.dup
-				board_copy[pos] = human_player
-				score = minimax(board_copy, true, cpu_player, human_player)
-				best_score = [score, best_score].min
-			end
-			best_score
-		end
-	end
-
-	# Returns available move positions
-	def available_moves(board_str = nil)
-		b = board_str || self.board
-		b.chars.each_with_index.map { |c, i| c == '-' ? i : nil }.compact
-	end
-
-	# Static check_status for minimax
-	def static_check_status(board)
-		lines = [
-			[0,1,2], [3,4,5], [6,7,8],
-			[0,3,6], [1,4,7], [2,5,8],
-			[0,4,8], [2,4,6]
-		]
-		lines.each do |line|
+		win_lines.each do |line|
 			values = line.map { |i| board[i] }
 			return "#{values[0]} wins" if values.uniq.length == 1 && values[0] != '-'
 		end
@@ -99,10 +33,71 @@ class GameState < ApplicationRecord
 		'ongoing'
 	end
 
-	# Play CPU move if it's CPU's turn
-	def play_cpu_move(cpu_player = 'O')
-		return false unless self.status == 'ongoing' && self.current_player == cpu_player
-		pos = best_move(cpu_player)
-		play_move(pos) if pos
+	def best_cpu_move(cpu_symbol = 'O')
+		moves = available_positions
+		best_score = -Float::INFINITY
+		best_position = nil
+		moves.each do |pos|
+			board_copy = board.dup
+			board_copy[pos] = cpu_symbol
+			score = minimax(board_copy, false, cpu_symbol, cpu_symbol == 'X' ? 'O' : 'X')
+			if score > best_score
+				best_score = score
+				best_position = pos
+			end
+		end
+		best_position
+	end
+
+	def minimax(board_state, maximizing, cpu_symbol, human_symbol)
+		result = static_game_status(board_state)
+		return 1 if result == "#{cpu_symbol} wins"
+		return -1 if result == "#{human_symbol} wins"
+		return 0 if result == 'draw'
+
+		if maximizing
+			best_score = -Float::INFINITY
+			available_positions(board_state).each do |pos|
+				board_copy = board_state.dup
+				board_copy[pos] = cpu_symbol
+				score = minimax(board_copy, false, cpu_symbol, human_symbol)
+				best_score = [score, best_score].max
+			end
+			best_score
+		else
+			best_score = Float::INFINITY
+			available_positions(board_state).each do |pos|
+				board_copy = board_state.dup
+				board_copy[pos] = human_symbol
+				score = minimax(board_copy, true, cpu_symbol, human_symbol)
+				best_score = [score, best_score].min
+			end
+			best_score
+		end
+	end
+
+	def available_positions(board_str = nil)
+		b = board_str || board
+		b.chars.each_with_index.map { |cell, idx| cell == '-' ? idx : nil }.compact
+	end
+
+	def static_game_status(board_state)
+		win_lines = [
+			[0, 1, 2], [3, 4, 5], [6, 7, 8],
+			[0, 3, 6], [1, 4, 7], [2, 5, 8],
+			[0, 4, 8], [2, 4, 6]
+		]
+		win_lines.each do |line|
+			values = line.map { |i| board_state[i] }
+			return "#{values[0]} wins" if values.uniq.length == 1 && values[0] != '-'
+		end
+		return 'draw' unless board_state.include?('-')
+		'ongoing'
+	end
+
+	def make_cpu_move(cpu_symbol = 'O')
+		return false unless status == 'ongoing' && current_player == cpu_symbol
+		pos = best_cpu_move(cpu_symbol)
+		make_move(pos) if pos
 	end
 end
